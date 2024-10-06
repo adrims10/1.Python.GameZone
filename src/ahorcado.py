43fee86518b9afcbc1dac794b2e032a69dc459b1
adrims10 <adrian.moreno30@outlook.es>
2024-10-06 15:56:23 +0200

    Actualizo ahorcado

--- src/ahorcado.py
+++ src/ahorcado.py
@@ -8,12 +8,31 @@ class Ahorcado:
         self.intentos_restantes = 10
 
     def elegir_palabra(self):
+        """ Funcion metodo para que la maquina elija una palabra aleatoria
+
+        Returns:
+            str: Devuelve una palabra aleatoria elegida por metodo import random
+        """
         return random.choice(self.palabras)
 
     def mostrar_estado(self):
+        """ Funcion metodo de la clase para definir el estado de la palabra conforme metemos letras
+
+        Returns:
+            srt: devuelve una cadena de str con la actualizacion de las letras acertadas o simulaciones de espacios
+        """
         return ''.join(letra if letra in self.letras_adivinadas else '_' for letra in self.palabra)
 
     def adivinar_letra(self, letra):
+        """definimos una funcion metodo de la clase en la que queremos sabe si la letra esta en la palabra elegida
+
+        Args:
+            letra (str): condicionamos si la letra se encuenta o no en la palabra, si ya la hemos dicho la mete en el set
+                        si no , printe  incorrecta y resta un intento
+
+        Returns:
+            str:devuelve si la letra es adivinada y la imprime y si no esta nos dice que no esta y nos resta un intento
+        """
         if letra in self.letras_adivinadas:
             print("Ya has adivinado esa letra. Intenta de nuevo.")
             return False
@@ -28,6 +47,7 @@ class Ahorcado:
             
 
     def juego(self):
+        
         patron = r"""
       .--------------------------------------------------.
      /                                                    \
@@ -40,7 +60,13 @@ class Ahorcado:
                \__________________________________/
                       \   |   |   |   |   /
                        \__|___|___|___|__/
+                
     """
+        """Definimos una funcion metodo la cual nos da las reglas del juego ahorcado
+        
+            Returns: el resultado del juego mientras no llegan a  intentos
+
+        """
         print(patron)
         print('Pista:Palabras relacionadas con el Bootcamp')
         
